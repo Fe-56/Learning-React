cbec44a1372f2ec5539702325fc09d813e108fe1
Fe-56 <limfuoen1999@gmail.com>
2024-05-17 10:39:37 +0800

    useState() hook

--- base/my-react-app/src/App.jsx
+++ base/my-react-app/src/App.jsx
@@ -7,6 +7,8 @@ import UserGreeting from './UserGreeting.jsx';
 import List from './List.jsx';
 import Button from './Button.jsx';
 import ProfilePicture from './ProfilePicture.jsx';
+import MyComponent from './MyComponent.jsx';
+import Counter from './Counter.jsx';
 
 function App() {
   const fruits = [
@@ -27,6 +29,8 @@ function App() {
 
   return (
     <div>
+      <Counter />
+      <MyComponent />
       <ProfilePicture />
       <Button />
       <List items={fruits} category="Fruits" />
---
+++ base/my-react-app/src/MyComponent.jsx
@@ -0,0 +1,34 @@
+// useState() hook
+import React, {useState} from 'react';
+
+function MyComponent() {
+  // every state should have its own useState() hook!
+  const [name, setName] = useState("Guest");
+  const [age, setAge] = useState(0);
+  const [isEmployed, setIsEmployed] = useState(false);
+  
+  const updateName = () => { 
+    setName("SpongeBob");
+  }
+
+  const updateAge = () => {
+    setAge(age + 1);
+  }
+
+  const toggleEmployedStatus = () => {
+    setIsEmployed(!isEmployed);
+  }
+
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <button onClick={updateName}>Set Name</button>
+      <p>Age: {age}</p>
+      <button onClick={updateAge}>Incremenet Age</button>
+      <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
+      <button onClick={toggleEmployedStatus}>Change employment</button>
+    </div>
+  );
+}
+
+export default MyComponent;
